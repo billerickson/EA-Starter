6873fd3d5a0caf0690b620c9a85fd05d8df71efc
Bill Erickson <bill.erickson@gmail.com>
2015-12-16 21:14:20 -0600

    dont update theme

--- functions.php
+++ functions.php
@@ -75,6 +75,29 @@ require get_template_directory() . '/inc/tha-theme-hooks.php';
 require get_template_directory() . '/inc/base.php';
 
 /**
+ * Dont Update the Theme
+ *
+ * If there is a theme in the repo with the same name, this prevents WP from prompting an update.
+ *
+ * @since  1.0.0
+ * @author Bill Erickson
+ * @link   http://www.billerickson.net/excluding-theme-from-updates
+ * @param  array $r Existing request arguments
+ * @param  string $url Request URL
+ * @return array Amended request arguments
+ */
+function ea_dont_update_theme( $r, $url ) {
+	if ( 0 !== strpos( $url, 'https://api.wordpress.org/themes/update-check/1.1/' ) )
+ 		return $r; // Not a theme update request. Bail immediately.
+ 	$themes = json_decode( $r['body']['themes'] );
+ 	$child = get_option( 'stylesheet' );
+	unset( $themes->themes->$child );
+ 	$r['body']['themes'] = json_encode( $themes );
+ 	return $r;
+ }
+add_filter( 'http_request_args', 'ea_dont_update_theme', 5, 2 ); 
+
+/**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * Priority 0 to make it available to lower priority callbacks.
