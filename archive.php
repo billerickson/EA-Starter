0b933ae91c462186048f87ae3ffe59ef4fd96324
Bill Erickson <bill.erickson@gmail.com>
2015-12-16 20:20:59 -0600

    set up the archive template

--- archive.php
+++ archive.php
@@ -1,51 +1,28 @@
 <?php
+
 /**
- * The template for displaying archive pages.
+ * Archive Body Class
  *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package ea
  */
+function ea_archive_body_class( $classes ) {
+	$classes[] = 'blog-archive';
+	return $classes;
+}
+add_filter( 'body_class', 'ea_archive_body_class' );
 
-get_header(); ?>
+/**
+ * Archive Header 
+ *
+ */
+function ea_archive_header() {
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	echo '<header class="page-header">';
+	the_archive_title( '<h1 class="page-title">', '</h1>' );
+	the_archive_description( '<div class="taxonomy-description">', '</div>' );
+	echo '</header>';
 
-		<?php
-		if ( have_posts() ) : ?>
+}
+add_action( 'tha_content_while_before', 'ea_archive_header' );
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+// Build the page
+require get_template_directory() . '/index.php';
