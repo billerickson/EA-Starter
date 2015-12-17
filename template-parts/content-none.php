20b612a2fe54a780bc5d4cda9222c7a8fc45d0f9
Bill Erickson <bill.erickson@gmail.com>
2015-12-16 21:08:20 -0600

    simplify content-none

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -15,12 +15,8 @@
 	</header><!-- .page-header -->
 
 	<div class="page-content">
-		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'ea' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-		<?php elseif ( is_search() ) : ?>
+		<?php if ( is_search() ) : ?>
 
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'ea' ); ?></p>
 			<?php
