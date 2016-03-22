19a990a06490e382e422743d189a1ecf5c4eb9a5
Bill Erickson <bill.erickson@gmail.com>
2016-03-22 13:23:16 -0500

    remove edit link

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -28,18 +28,4 @@
 			tha_entry_content_after();
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					esc_html__( 'Edit %s', 'ea' ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
