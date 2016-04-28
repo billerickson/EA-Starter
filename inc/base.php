f1116e79919bc77c509a738f9fb4bb3b48d30650
Bill Erickson <bill.erickson@gmail.com>
2016-04-28 09:35:36 -0500

    simplify entry-meta

--- inc/base.php
+++ inc/base.php
@@ -121,25 +121,12 @@ add_filter( 'ea_content_template', 'ea_content_template' );
  */
 function ea_entry_meta() {
 
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
-	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'ea' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'ea' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-	echo apply_filters( 'ea_entry_meta', '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>' );
+	$author = 'By <a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" class="entry-author">' . get_the_author() . '</a> ';
+	$posted_on = 'on <time class="entry-date" datetime="' . get_the_time( 'U' ) . '">' . get_the_date() . '</time> ';
+	$comments  = 'with <a href="' . get_comments_link() . '" class="entry-comments"> ' . get_comments_number() . ' ' . _n( 'Comment', 'Comments', get_comments_number(), 'ea' ) . '</a>';
+	$entry_meta = $author . $posted_on . $comments;
+
+	echo apply_filters( 'ea_entry_meta', $entry_meta );
 
 }
 
