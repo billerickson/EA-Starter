a8b15bfd56d16b4a730c105af779670070b281d2
Bill Erickson <bill.erickson@gmail.com>
2016-07-15 15:56:20 -0500

    move entry meta/footer to template part

--- inc/loop.php
+++ inc/loop.php
@@ -86,57 +86,6 @@ function ea_archive_header() {
 add_action( 'tha_content_while_before', 'ea_archive_header' );
 
 /**
- * Entry Meta
- *
- */
-function ea_entry_meta() {
-
-	$entry_meta = '';
-
-	if( 'post' == get_post_type() )  {
-
-		$author = 'By <a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" class="entry-author">' . get_the_author() . '</a> ';
-		$posted_on = 'on <time class="entry-date" datetime="' . get_the_time( 'U' ) . '">' . get_the_date() . '</time> ';
-		$comments  = 'with <a href="' . get_comments_link() . '" class="entry-comments"> ' . get_comments_number() . ' ' . _n( 'Comment', 'Comments', get_comments_number(), 'ea' ) . '</a>';
-		$entry_meta = $author . $posted_on . $comments;
-
-	}
-
-	$entry_meta = apply_filters( 'ea_entry_meta', $entry_meta );
-	
-	if( $entry_meta )
-		echo '<div class="entry-meta">' . $entry_meta . '</div>';
-
-}
-add_action( 'tha_entry_content_before', 'ea_entry_meta' );
-
-/**
- * Entry Footer 
- *
- */
-function ea_entry_footer() {
-
-	$output = '';
-	
-	// Hide category and tag text for pages.
-	if ( 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'ea' ) );
-		if ( $categories_list ) {
-			$output .= '<span class="entry-categories">' . esc_html__( 'Posted in', 'ea' ) . $categories_list . '</span>';
-		}
-		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'ea' ) );
-		if ( $tags_list ) {
-			$output .= '<span class="entry-tags">' . esc_html__( 'Tagged', 'ea' ) . $tags_list . '</span>';
-		}
-	}
-
-	echo apply_filters( 'ea_entry_footer', $output );
-}
-add_action( 'tha_entry_content_after', 'ea_entry_footer' );
-
-/**
  * Archive Navigation 
  *
  */
--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -17,6 +17,13 @@
 		} else {
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		}
+
+		// Entry Meta
+		$author = 'By <a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" class="entry-author">' . get_the_author() . '</a> ';
+		$posted_on = 'on <time class="entry-date" datetime="' . get_the_time( 'U' ) . '">' . get_the_date() . '</time> ';
+		$comments  = 'with <a href="' . get_comments_link() . '" class="entry-comments"> ' . get_comments_number() . ' ' . _n( 'Comment', 'Comments', get_comments_number(), 'ea' ) . '</a>';
+		echo '<div class="entry-meta">' . $author . $posted_on . $comments . '</div>';
+
 		?>
 		<?php tha_entry_top(); ?>
 	</header><!-- .entry-header -->
@@ -48,5 +55,24 @@
 
 	<footer class="entry-footer">
 	<?php tha_entry_bottom(); ?>
+	
+	<?php
+	// Entry Footer
+	$output = '';
+	
+	$categories_list = get_the_category_list( esc_html__( ', ', 'ea' ) );
+	if ( $categories_list ) {
+		$output .= '<span class="entry-categories">' . esc_html__( 'Posted in', 'ea' ) . $categories_list . '</span>';
+	}
+
+	$tags_list = get_the_tag_list( '', esc_html__( ', ', 'ea' ) );
+	if ( $tags_list ) {
+		$output .= '<span class="entry-tags">' . esc_html__( 'Tagged', 'ea' ) . $tags_list . '</span>';
+	}
+	
+	if( $output )
+		echo '<p>' . $output . '</p>';
+	?>
+
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
