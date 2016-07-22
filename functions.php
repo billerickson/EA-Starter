24e60c20a3b8324886e259801ca3dde7b288bd33
Bill Erickson <bill.erickson@gmail.com>
2016-07-22 08:36:27 -0700

    add formats dropdown to tinymce

--- functions.php
+++ functions.php
@@ -147,3 +147,37 @@ function ea_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'ea_scripts' );
+
+/** 
+ * Add "Styles" drop-down to TinyMCE
+ *
+ * @since 1.0.0
+ * @param array $buttons
+ * @return array
+ */
+function ea_mce_editor_buttons( $buttons ) {
+	array_unshift( $buttons, 'styleselect' );
+	return $buttons;
+}
+add_filter( 'mce_buttons_2', 'ea_mce_editor_buttons' );
+
+/** 
+ * Add styles/classes to the TinyMCE "Formats" drop-down 
+ *
+ * @since 1.0.0
+ * @param array $settings
+ * @return array
+ */
+function ea_mce_before_init( $settings ) {
+
+	$style_formats = array(
+		array(
+			'title'    => 'Button',
+			'selector' => 'a',
+			'classes'  => 'button',
+		),
+	);
+	$settings['style_formats'] = json_encode( $style_formats );
+	return $settings;
+}
+add_filter( 'tiny_mce_before_init', 'ea_mce_before_init' );
