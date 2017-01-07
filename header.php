471bf857ff8cfc848085905146540312a6248406
Bill Erickson <bill.erickson@gmail.com>
2017-01-07 08:15:04 -0600

    use correct closing tag on .site-title

--- header.php
+++ header.php
@@ -30,7 +30,7 @@ echo '<div class="site-container">';
 
 		echo '<div class="site-branding">';
 		$logo_tag = ( apply_filters( 'ea_h1_site_title', false ) || ( is_front_page() && is_home() ) ) ? 'h1' : 'p';
-		echo '<' . $logo_tag . ' class="site-title"><a href="' . esc_url( home_url() ) . '" rel="home">' . get_bloginfo( 'name' ) . '</a></h1>';
+		echo '<' . $logo_tag . ' class="site-title"><a href="' . esc_url( home_url() ) . '" rel="home">' . get_bloginfo( 'name' ) . '</a></' . $logo_tag . '>';
 		echo '<p class="site-description">' . get_bloginfo( 'description' ) . '</p>';
 		echo '</div>';
 
