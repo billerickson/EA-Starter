4a2548aaa8f61f2e2341493dc5974badec082cda
Bill Erickson <bill.erickson@gmail.com>
2017-04-18 16:17:05 -0500

    add filter to display sidebar when no sidebar
    Useful for when you dont want sidebar widgets, but do want to hook a function to tha_sidebar_top or tha_sidebar_bottom. See: https://gist.github.com/billerickson/cc438de4c241d277d97327d2b8d2d988`

--- sidebar.php
+++ sidebar.php
@@ -8,22 +8,24 @@
  * @license      GPL-2.0+
  */
 
-if( ! function_exists( 'ea_page_layout' ) )
+if ( ! function_exists( 'ea_page_layout' ) )
 	return;
 
 $layout = ea_page_layout();
-if( ! in_array( $layout, array( 'content-sidebar', 'sidebar-content' ) ) )
+if ( ! in_array( $layout, array( 'content-sidebar', 'sidebar-content' ) ) )
 	return;
 
 $sidebar = apply_filters( 'ea_sidebar', 'primary-sidebar' );
-if ( ! is_active_sidebar( $sidebar ) )
+$display = is_active_sidebar( $sidebar );
+if ( ! apply_filters( 'ea_display_sidebar', $display ) ))
 	return;
 
 tha_sidebars_before();
 $classes = apply_filters( 'ea_sidebar_class', array( 'col-md-3' ) );
 echo '<div class="' . join( ' ', $classes ) . '"><aside class="sidebar-primary" role="complementary">';
 	tha_sidebar_top();
-	dynamic_sidebar( $sidebar );
+	if ( is_active_sidebar( $sidebar ) )
+		dynamic_sidebar( $sidebar );
 	tha_sidebar_bottom();
 echo '</aside></div>';
 tha_sidebars_after();
