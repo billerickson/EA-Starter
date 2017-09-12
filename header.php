6728600dc1c66b09500e3cd2128319131a9d4dd0
Bill Erickson <bill.erickson@gmail.com>
2017-09-12 10:38:16 -0500

    disable description by default

--- header.php
+++ header.php
@@ -31,7 +31,7 @@ echo '<div class="site-container">';
 		echo '<div class="site-branding">';
 		$logo_tag = ( apply_filters( 'ea_h1_site_title', false ) || ( is_front_page() && is_home() ) ) ? 'h1' : 'p';
 		echo '<' . $logo_tag . ' class="site-title"><a href="' . esc_url( home_url() ) . '" rel="home">' . get_bloginfo( 'name' ) . '</a></' . $logo_tag . '>';
-		echo '<p class="site-description">' . get_bloginfo( 'description' ) . '</p>';
+		//echo '<p class="site-description">' . get_bloginfo( 'description' ) . '</p>';
 		echo '</div>';
 
 		tha_header_bottom();
