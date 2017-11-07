a702a62643fe2476c9f9d5caa1ab0c61922fdecf
Bill Erickson <bill.erickson@gmail.com>
2017-11-07 14:19:48 -0600

    dont use hooks on archive partial

--- partials/archive.php
+++ partials/archive.php
@@ -11,17 +11,11 @@
 echo '<article class="' . join( ' ', get_post_class() ) . '">';
 
 	echo '<header class="entry-header">';
-		tha_entry_top();
+		ea_entry_title();
 	echo '</header>';
 
 	echo '<div class="entry-content">';
-		tha_entry_content_before();
 		the_excerpt();
-		tha_entry_content_after();
 	echo '</div>';
-
-	echo '<footer class="entry-footer">';
-		tha_entry_bottom();
-	echo '</footer>';
 
 echo '</article>';
