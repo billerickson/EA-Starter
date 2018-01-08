b9f66706def48d11c01c9772c00119cab0cb36b9
Bill Erickson <bill.erickson@gmail.com>
2018-01-08 15:44:56 -0600

    rename site title area to match genesis

--- header.php
+++ header.php
@@ -27,7 +27,7 @@ echo '<div class="site-container">';
 	echo '<header class="site-header" role="banner"><div class="wrap">';
 		tha_header_top();
 
-		echo '<div class="site-branding">';
+		echo '<div class="title-area">';
 		$logo_tag = ( apply_filters( 'ea_h1_site_title', false ) || ( is_front_page() && is_home() ) ) ? 'h1' : 'p';
 		echo '<' . $logo_tag . ' class="site-title"><a href="' . esc_url( home_url() ) . '" rel="home">' . get_bloginfo( 'name' ) . '</a></' . $logo_tag . '>';
 		//echo '<p class="site-description">' . get_bloginfo( 'description' ) . '</p>';
