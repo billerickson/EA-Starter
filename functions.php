42c7e48730828a57b8fb0eb459b3eac04d20807f
Bill Erickson <bill.erickson@gmail.com>
2018-02-23 13:53:09 -0600

    optimize for gutenberg

--- functions.php
+++ functions.php
@@ -101,6 +101,7 @@ function ea_setup() {
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'align-wide' );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
