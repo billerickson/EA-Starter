6197a657894a6de33dc2af692e51027fed3ed557
Bill Erickson <bill.erickson@gmail.com>
2019-02-08 15:14:14 -0600

    mobile navigation

--- assets/js/src/global.js
+++ assets/js/src/global.js
@@ -1,5 +1,13 @@
 // @codekit-prepend "smoothscroll.js"
 
 jQuery(function($){
+
+	// Mobile menu
+	$('.mobile-menu-toggle').click(function(){
+		$('body').toggleClass('mobile-menu-expanded');
+	});
+	$('.submenu-expand').click(function(){
+		$(this).parent().toggleClass('submenu-active');
+	});
 	
 });
