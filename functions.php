a9e25b7ed0b1232f3eb34c218e73f4beebbeaf3d
Bill Erickson <bill.erickson@gmail.com>
2019-05-29 08:45:06 -0500

    wpforms, use button styling

--- functions.php
+++ functions.php
@@ -20,12 +20,13 @@ require get_template_directory() . '/inc/login-logo.php';
 require get_template_directory() . '/inc/helper-functions.php';
 require get_template_directory() . '/inc/navigation.php';
 //require get_template_directory() . '/inc/sidebar-layouts.php';
+//require get_template_directory() . '/inc/custom-logo.php';
 require get_template_directory() . '/inc/loop.php';
 require get_template_directory() . '/inc/tinymce.php';
 require get_template_directory() . '/inc/disable-editor.php';
 require get_template_directory() . '/inc/amp.php';
 require get_template_directory() . '/inc/instantpage.php';
-//require get_template_directory() . '/inc/custom-logo.php';
+require get_template_directory() . '/inc/wpforms.php';
 
 /**
  * Enqueue scripts and styles.
