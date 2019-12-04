5a44bffef228b946fb1c2ddb9fb06fd67bf4f577
Bill Erickson <bill.erickson@gmail.com>
2019-12-04 13:33:54 -0600

    add missing meta tags

--- inc/wordpress-cleanup.php
+++ inc/wordpress-cleanup.php
@@ -8,28 +8,40 @@
  * @license      GPL-2.0+
 **/
 
- /**
-  * Dont Update the Theme
-  *
-  * If there is a theme in the repo with the same name, this prevents WP from prompting an update.
-  *
-  * @since  1.0.0
-  * @author Bill Erickson
-  * @link   http://www.billerickson.net/excluding-theme-from-updates
-  * @param  array $r Existing request arguments
-  * @param  string $url Request URL
-  * @return array Amended request arguments
-  */
- function ea_dont_update_theme( $r, $url ) {
- 	if ( 0 !== strpos( $url, 'https://api.wordpress.org/themes/update-check/1.1/' ) )
-  		return $r; // Not a theme update request. Bail immediately.
-  	$themes = json_decode( $r['body']['themes'] );
-  	$child = get_option( 'stylesheet' );
- 	unset( $themes->themes->$child );
-  	$r['body']['themes'] = json_encode( $themes );
-  	return $r;
-  }
- add_filter( 'http_request_args', 'ea_dont_update_theme', 5, 2 );
+/**
+* Dont Update the Theme
+*
+* If there is a theme in the repo with the same name, this prevents WP from prompting an update.
+*
+* @since  1.0.0
+* @author Bill Erickson
+* @link   http://www.billerickson.net/excluding-theme-from-updates
+* @param  array $r Existing request arguments
+* @param  string $url Request URL
+* @return array Amended request arguments
+*/
+function ea_dont_update_theme( $r, $url ) {
+	if ( 0 !== strpos( $url, 'https://api.wordpress.org/themes/update-check/1.1/' ) )
+		return $r; // Not a theme update request. Bail immediately.
+	$themes = json_decode( $r['body']['themes'] );
+	$child = get_option( 'stylesheet' );
+	unset( $themes->themes->$child );
+	$r['body']['themes'] = json_encode( $themes );
+	return $r;
+}
+add_filter( 'http_request_args', 'ea_dont_update_theme', 5, 2 );
+
+/**
+ * Header Meta Tags
+ *
+ */
+function ea_header_meta_tags() {
+	echo '<meta charset="' . get_bloginfo( 'charset' ) . '">';
+	echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
+	echo '<link rel="profile" href="http://gmpg.org/xfn/11">';
+	echo '<link rel="pingback" href="' . get_bloginfo( 'pingback_url' ) . '">';
+}
+add_action( 'wp_head', 'ea_header_meta_tags' );
 
 /**
  * Dequeue jQuery Migrate
