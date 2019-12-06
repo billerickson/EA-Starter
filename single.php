abf3f77c4f2469c03c1432d52daf95cccc2eec42
Bill Erickson <bill.erickson@gmail.com>
2019-12-06 06:54:05 -0600

    remove unused functionality, see #18

--- single.php
+++ single.php
@@ -37,31 +37,6 @@ function ea_single_after_entry() {
 	// Sharing
 	do_action( 'ea_entry_footer_share' );
 
-	// Author Box
-	ea_author_box();
-
-	// Newsletter signup
-	$form_id = get_option( 'options_ea_newsletter_form' );
-	if( $form_id && function_exists( 'wpforms_display' ) )
-		wpforms_display( $form_id, true, true );
-
-	// Related Posts
-	$loop = new WP_Query( [
-		'posts_per_page'	=> 3,
-		'post__not_in'		=> [ get_the_ID() ],
-		'category_name'		=> ea_first_term( [ 'field' => 'slug' ] ),
-	] );
-	if( $loop->have_posts() ):
-		echo '<section class="post-listing-block layout3">';
-		echo '<header><h2>Other articles you may like</h2></header>';
-		while( $loop->have_posts() ):
-			$loop->the_post();
-			get_template_part( 'partials/archive' );
-		endwhile;
-		echo '</section>';
-		wp_reset_postdata();
-	endif;
-	echo '</div>';
 }
 add_action( 'tha_content_while_after', 'ea_single_after_entry', 8 );
 
